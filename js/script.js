d57eb33f5ec40da456454e69b91d7b94e19e3a21
Michał Puchała <michalpuchala86@gmail.com>
2025-04-28 16:12:49 +0200

    Working test game

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage(
-  "Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!"
-);
+console.log("Wylosowana liczba to: " + randomNumber);
 
-let playerMove = `papier`;
+let computerMove = "nieznany ruch";
 
-printMessage(
-  "Zagrałem" + playerMove + "! Jeśli Twój ruch to kamień, to wygrywasz"
-);
+if (randomNumber == "1") {
+  computerMove = "kamień";
+} else if (randomNumber == "2") {
+  computerMove = "papier";
+} else if (randomNumber == "3") {
+  computerMove = "nożyce";
+}
+
+printMessage("Mój ruch to: " + computerMove);
+
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Gracz wpisał: " + playerInput);
+let playerMove = "nieznany ruch";
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nożyce";
+}
+printMessage("Twój ruch to: " + playerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "kamień" && playerMove == "nożyce") {
+  printMessage("Komputer wygrywa!");
+} else if (computerMove == "papier" && playerMove == "kamień") {
+  printMessage("Komputer wygrywa!");
+} else if (computerMove == "nożyce" && playerMove == "papier") {
+  printMessage("Komputer wygrywa!");
+} else if (computerMove == playerMove) {
+  printMessage("Remis!");
+} else if (playerMove == "nieznany ruch") {
+  printMessage("Błąd wybierz ponownie");
+}
